4625ad8ded3aa705433ba4a8e36eedb9b25c1534
Naman Mukund <namanmukund@gmail.com>
2020-01-05 23:37:16 +0530

    Updates

---
+++ precache-manifest.38f1b917469f9d0e066001c096a12d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cf0f0b125d55a91aa9027db9c3fbe6",
+    "url": "/music-shit/index.html"
+  },
+  {
+    "revision": "7cfbdb77574034998917",
+    "url": "/music-shit/static/css/main.f57eb2e5.chunk.css"
+  },
+  {
+    "revision": "6bba058c2d0f3bb6b1b4",
+    "url": "/music-shit/static/js/2.664e8f3e.chunk.js"
+  },
+  {
+    "revision": "7cfbdb77574034998917",
+    "url": "/music-shit/static/js/main.a14bf8d8.chunk.js"
+  },
+  {
+    "revision": "de057d089830a24d95c0",
+    "url": "/music-shit/static/js/runtime-main.00317dc7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/music-shit/static/media/logo.25bf045c.svg"
+  }
+]);
